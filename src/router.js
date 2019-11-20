7d928a43d0d28ef786e8b5e623b839f695601807
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-11-20 06:35:40 -0800

    google auth working

--- src/router.js
+++ src/router.js
@@ -1,10 +1,40 @@
 import passport from 'passport';
 import FacebookStrategy from 'passport-facebook';
+import { OAuth2Strategy as GoogleStrategy} from 'passport-google-oauth';
 import server from './server';
 import prisma from './prisma';
 import hashPassword from './utils/hashPassword';
 import generatePassword from './utils/generatePassword';
 import generateAuthToken from './utils/generateAuthToken';
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: `${process.env.API_URL}/auth/google/callback`,
+  scope: ['https://www.googleapis.com/auth/plus.login', 'profile'],
+},
+async function(accessToken, refreshToken, profile, done) {
+  const { displayName, emails, photos } = profile;
+  const name = displayName;
+  const email = emails[0].value;
+  const avatar = photos[0].value;
+
+  let user;
+  const userExists = await prisma.exists.User({ email });
+  if ( userExists ) user = await prisma.query.user({ where: { email }});
+  else {
+    const password = generatePassword();
+    const hashedPassword = await hashPassword(password);
+    user = await prisma.mutation.createUser({
+      data: {
+        name, email, avatar: avatar ? avatar :'no-avatar.jpg', password: hashedPassword
+      }
+    });
+  }
+  user.token = generateAuthToken(user.id);
+  return done(null, user );
+}
+));
 
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_CLIENT_ID,
@@ -40,18 +70,24 @@ async function(accessToken, refreshToken, profile, cb) {
 
 server.express.use(passport.initialize());
 
+server.express.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+server.express.get('/auth/google/callback',
+passport.authenticate('google', { session: false }), (req, res) => {
+  const { user } = req;
+  console.log({ user });
+  res.send('Google Auth was good');
+});
+
 server.express.get('/auth/facebook', passport.authenticate('facebook', {
   authType: 'rerequest',
   scope: ['email', 'public_profile'],
 }));
 
 server.express.get('/auth/facebook/callback', passport.authenticate('facebook', { session: false }), (req, res) => {
-  const {user, token } = req;
-  console.log({ user, token });
+  const {user } = req;
+  console.log({ user });
   res.send('Auth was good');
-});
-
-server.express.get('/route', async (req, res, next) => {
-  res.send('hello bazina');
 });
 
