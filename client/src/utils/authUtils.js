b8178c5e6dbe49010534c8fccb3a28f2a9540a38
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-11-24 12:25:40 -0800

    init utils

---
+++ client/src/utils/authUtils.js
@@ -0,0 +1,19 @@
+/** THIS FILE ENCAPSULATE ALL THE UTILS NEEDED FOR AUTHENTICATION */
+
+class AuthUtils {
+  constructor() {}
+
+  isLoggedIn() {}
+
+  signUp() {}
+
+  login() {}
+
+  logout() {}
+
+  getToken() {}
+}
+
+const authUtils = new AuthUtils();
+
+export default authUtils;
