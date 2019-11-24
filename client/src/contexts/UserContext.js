4ea5557be84211a54aec82c35c9da5983f6d493b
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-11-24 12:18:14 -0800

    created UserContext

--- client/src/contexts/UserContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const UserContext = React.createContext({
-  name: 'Bazinga'
-});
-
-export const UserProvider = UserContext.Provider;
-export const UserConsumer = UserContext.Consumer;
-export default UserContext;
