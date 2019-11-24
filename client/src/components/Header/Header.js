dfd7dba0ae63f6b2d9e8ec8b987b64b682f8dc57
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-11-24 12:25:11 -0800

    linked Header to the mainDialogs context

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,38 @@
 import React, { useContext } from 'react';
-import UserContext from '../../contexts/UserContext';
+import { AuthContext } from '../../contexts/AuthContext';
+import { DialogContext } from '../../contexts/DialogContext';
+import Button from '../lib/Button';
 
 const Header = () => {
-  const user = useContext(UserContext);
-  console.log({ user });
+  const { user, signup, login } = useContext(AuthContext);
+  const { setDialog } = useContext(DialogContext);
+
+  function openSignupDialog() {
+    const opts = {
+      type: 'signup',
+      submitHandler: signup
+    };
+    setDialog(opts);
+  }
+
+  function openLoginDialog() {
+    const opts = {
+      type: 'login',
+      submitHandler: login
+    };
+    setDialog(opts);
+  }
 
   return (
-    <header>Header</header>
+    <header className='header'>
+      <div className="left">
+        <h4 className="title">Screen Reader</h4>
+      </div>
+      <div className="right">
+        <Button onClick={openLoginDialog}>Login</Button>
+        <Button onClick={openSignupDialog}>Signup</Button>
+      </div>
+    </header>
   );
 };
 
