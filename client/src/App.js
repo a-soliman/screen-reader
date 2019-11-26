0dff07db674368f0561c5572b5de927f519de543
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-11-25 21:25:22 -0800

    init react-appolo

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import GlobalStateProvider from './contexts/GlobalState';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import './App.scss';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000'
+});
+
 function App() {
   return (
-    <GlobalStateProvider>
-      <Header />
-      <Footer />
-    </GlobalStateProvider>
+    <ApolloProvider client={client}>
+      <GlobalStateProvider>
+        <Header />
+        <Footer />
+      </GlobalStateProvider>
+    </ApolloProvider>
+
   );
 }
 
